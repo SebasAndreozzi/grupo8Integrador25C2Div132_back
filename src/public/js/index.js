cd0728596baf0b805c23deb7f0ba72b9817c2992
SebasAndreozzi <sebastian.javier.andreozzi@gmail.com>
2025-11-27 13:12:01 -0300

    Agrego funcinalidad de carrito (Falta Fincalizar compra)

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,5 +1,10 @@
 let contenedorProductos = document.getElementById("contenedor-productos");
+let contenedorCarrito = document.getElementById("contenedor-carrito");
 const url = "http://localhost:3000/api/productos"
+
+let carrito = [];
+if(JSON.parse(localStorage.getItem("carrito"))){
+    carrito = JSON.parse(localStorage.getItem("carrito"));}
 
 async function obtenerProductos()
 {
@@ -17,6 +22,7 @@ async function obtenerProductos()
         console.log(error);
     }
 }
+
 function mostrarProductos(array)
 {
     let htmlProducto ="";
@@ -27,7 +33,7 @@ function mostrarProductos(array)
                 <img src="${producto.img}" alt="${producto.nombre}">
                 <h3>${producto.nombre}</h3>
                 <p>$${producto.precio}</p>
-                <input class="button" type="button" id="idProducto" value="Agregar al carrito">
+                <button class="button" data-producto='${JSON.stringify(producto)}' onclick="agregarACarrito(this.dataset.producto)">Agregar al carrito</button>
             </div>
         `;
 
@@ -43,9 +49,65 @@ function mostrarProductos(array)
     accesorio.addEventListener("click", event =>{
         filtrarPorTipo(array, accesorio.textContent);
     })
+
+    let carrito = document.getElementById("carrito");
+    carrito.addEventListener("click", (event) =>{
+        mostrarCarrito(array);
+    })
    
 }
 
+/*====================
+  Funcines de carrito
+=====================*/
+function agregarACarrito(producto){ 
+    carrito.push(producto);
+}
+
+function mostrarCarrito(){
+
+    let htmlCarrito ="";
+
+  carrito.forEach((producto, i)=> {
+        let prod = JSON.parse(producto);
+
+        htmlCarrito += `
+            <div class= "card-producto">
+                <img src="${prod.img}" alt="${prod.nombre}">
+                <h3>${prod.nombre}</h3>
+                <p>$${prod.precio}</p>
+                <button class="button" onclick="eliminarElemento(${i})">Eliminar</button>
+            </div>
+        `; 
+    });
+
+    htmlCarrito += `<button class=button>Finalizar compra</button>`
+
+    if(carrito.length > 0){
+        contenedorCarrito.innerHTML = htmlCarrito;
+    }
+
+}
+
+function eliminarElemento(indice){
+    carrito.splice(indice, 1);
+
+    if(carrito.length === 0){
+        vaciarCarrito();
+
+    }else{
+        mostrarCarrito();
+    }
+}
+
+function vaciarCarrito(){
+    carrito = [];
+    contenedorCarrito.innerHTML = "";
+}
+
+/*===================
+  Funcines de filtro
+====================*/
 function filtrarPorTipo(array, tipo){
     let htmlProducto ="";
 
@@ -56,13 +118,14 @@ function filtrarPorTipo(array, tipo){
                 <img src="${producto.img}" alt="${producto.nombre}">
                 <h3>${producto.nombre}</h3>
                 <p>$${producto.precio}</p>
-                <input class="button" type="button" id="idProducto" value="Agregar al carrito">
+                <button class="button" onclick="agregarACarrito(${array})">Agregar al carrito</button>
             </div>
         `;
 
     });
     contenedorProductos.innerHTML = htmlProducto;
 }
+
 
 function init() {
     obtenerProductos();
